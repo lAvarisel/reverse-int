db6561d1c288bb69a187fda4da24dafe3deba8fd
Dmitry Melnikov <dmitry.melni@gmail.com>
2020-10-24 23:25:56 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    if(n.toString().split('')[0] === '-') {
+        return `${n.toString().split('').reverse().join().replace(/,/g,'').replace(/-/g,'')}`
+    } else  return n.toString().split('').reverse().join().replace(/,/g,'')
 }
